7f89f4857adce9eb702da4df4db0d0685a767129
洪远麒 <415453941@qq.com>
2020-03-02 23:47:18 +0800

    feat:add prod host

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,8 @@
+// dev
+const isDev = /^(192\.168|localhost)/.test(window.location.host);
+
 export default {
-  host: 'http://localhost:7001',
+  host: isDev ? 'http://localhost:7001' : 'https://api.baishiup.com',
   qiniuUrl: 'https://up-z2.qiniup.com',
   cdnUrl: 'https://cdn.baishiup.com'
 };
